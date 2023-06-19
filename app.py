0911136d0009d869f0a1af6776c73b6453fae873
junsaga <124113134+junsaga@users.noreply.github.com>
2023-06-19 18:00:12 +0900

    Commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_restful import Api
+from config import Config
+from resources.recipe import RecipeListResource, RecipeResource
+from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource, jwt_blocklist
+from flask_jwt_extended import JWTManager
+
+
+app = Flask(__name__)
+
+# 환경변수 셋팅
+app.config.from_object(Config)
+
+# JWT 매니저 초기화 
+jwt = JWTManager(app)
+
+# 로그아웃된 토큰으로 요청하는 경우! 이 경우는 비정상적인 경우
+# 이므로, jwt 가 알아서 처리하도록 코드작성.
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload['jti']
+    return jti in jwt_blocklist
+
+
+api = Api(app)
+
+# 경로와 API동작코드(Resource) 를 연결한다.
+api.add_resource( RecipeListResource, '/recipes')
+api.add_resource( RecipeResource   ,  '/recipes/<int:recipe_id>' )
+api.add_resource( UserRegisterResource, '/user/register' )
+api.add_resource( UserLoginResource  , '/user/login')
+api.add_resource( UserLogoutResource , '/user/logout')
+
+if __name__ == '__main__' :
+    app.run()
---
+++ resources/utils.py
@@ -0,0 +1,20 @@
+from passlib.hash import pbkdf2_sha256
+from config import Config
+# 1. 원문 비밀번호를 단방향으로 암호화 하는 함수
+def hash_password(original_password) :
+    password = pbkdf2_sha256.hash(original_password + Config.SALT)
+    return password
+
+# 2. 유저가 입력한 비번이, 맞는지 체크하는 함수
+def check_passowrd(original_password,hashed_password) :
+    pbkdf2_sha256.verify(original_password+Config.SALT,hash_password)
+    return check
+
+
+
+
+
+
+
+
+
